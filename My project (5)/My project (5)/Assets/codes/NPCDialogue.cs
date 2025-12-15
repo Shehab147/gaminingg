bc162efa01a2759911bbf8e67589b94155a5fb16
Rew990 <rawan244030@bue.edu.eg>
2025-12-15 05:08:42 +0200

    15/12-5:08am

---
+++ My project (5)/My project (5)/Assets/codes/NPCDialogue.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCDialogue : MonoBehaviour
+{
+    public Dialogue dialogueManager; //a variable that stores the Dialogue script that is attached to the Dialogue Manager gameObject 
+   
+    //This can be used as a more elegant alternative to FindObjectOfType‹Dialogue>()
+    
+    
+    void OnTriggerEnter2D(Collider2D other){
+        if (other.tag == "Player") //if the player is the one that triggers the collider, then
+        { 
+            string[] dialogue = { "Echo: Ava...You have reached the final level .... A closer step to the end",
+            "AVA: What is this place?",
+            "Echo: This is the Death Maze. Stay alert as time is counting down .Every Second Matters",
+            "AVA : WHAT!! AM GOING TO DIE ?",
+            "ECHO : CALM DOWN..AND FOCUS . Watch of moving platforms ,they wont wait for you ",
+            "AVA : OK.. WHAT ELSE !! ",
+            "Echo: Second .. you should break the wall to open different pathes .",
+            "AVA : I guess i can do that ",
+            "Echo : Choose wisely ,some paths have  objects to increase time or give you more lives ",
+            "Echo :  Few signs warns from  exploding traps , as well as flying spikes ",
+            "Echo :Final Surprise ...... You will face the MasterMind by the end make Sure to use your punches for a victory ",
+            "AVA: Sure its just a click of F .IM READY ",}; //specify the dialogue between the player and the character
+            dialogueManager.SetSentences(dialogue); //set the sentences array in the Dialogue script to above array 
+            dialogueManager.StartCoroutine(dialogueManager.TypeDialogue()); //start the coroutine of TypeDialogue), which in turn starts the dialogue
+            Destroy (GetComponent<BoxCollider2D>(), 5f); //destroys the NPC's triggered box collider so
+            //the player doesn't accidentally re-trigger the conversation 
+        }
+    }
+
+}
+
